f28194270dd0ee30215d0a0a978b45c375443989
Giovanni Romaña <giovanni.cuesta@t-evolvers.com>
2023-04-04 23:57:13 -0500

    Test vercel deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import styles from '../styles/Home.module.css';
 import CodeIcon from '@/assets/CodeIcon';
+import DownloadIcon from '@/assets/DownloadIcon';
 
 export default function Home() {
   return (
     <div className={styles.main_container}>
       <CodeIcon />
+      <DownloadIcon />
     </div>
   );
 }
