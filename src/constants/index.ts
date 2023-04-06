f7c0608cea5d7d9691d8e8baf556951a3f55038e
Giovanni Romaña <giovanni.cuesta@t-evolvers.com>
2023-04-05 23:03:35 -0500

    Create home and add button for download cv

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const CV_URL =
+  'https://gio-personal-portfolio.s3.amazonaws.com/CV-Giovanni_Roma%C3%B1a_Cuesta-2023.pdf';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styles from '../styles/Home.module.css';
-import CodeIcon from '@/assets/CodeIcon';
-import DownloadIcon from '@/assets/DownloadIcon';
+import HomeText from '@/components/HomeText';
 import SocialMedias from '@/components/SocialMedias';
 
 export default function Home() {
   return (
     <div className={styles.main_container}>
       <SocialMedias />
+      <HomeText />
     </div>
   );
 }
